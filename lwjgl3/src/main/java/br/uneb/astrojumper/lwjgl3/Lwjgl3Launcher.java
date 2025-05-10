c613517c1f23a90bd2ac2a603ece3ecd4b26ddaa
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2025-05-09 22:30:51 -0300

    add textures

--- lwjgl3/src/main/java/br/uneb/astrojumper/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/br/uneb/astrojumper/lwjgl3/Lwjgl3Launcher.java
@@ -28,10 +28,13 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 
-        configuration.setWindowedMode(800, 500);
+        configuration.setWindowedMode(1100, 680);
+        configuration.setResizable(false);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon("icon64.png");
+
+        configuration.setPauseWhenLostFocus(true);
         return configuration;
     }
 }
