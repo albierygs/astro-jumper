d8dda0fcf215207f5e83d63b98ff5380fab4a5d2
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2025-05-16 14:14:39 -0300

    refactor(core, assets): code refactor

---
+++ assets/maps/assets/platformerPack-industrial-tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="platformerPack-industrial-tilesheet" tilewidth="68" tileheight="68" spacing="2" margin="1" tilecount="112" columns="14">
+ <image source="platformerPack-industrial-tilesheet.png" width="980" height="560"/>
+</tileset>
--- core/src/main/java/br/uneb/astrojumper/AstroJumper.java
+++ core/src/main/java/br/uneb/astrojumper/AstroJumper.java
@@ -1,21 +1,26 @@
 package br.uneb.astrojumper;
 
 import br.uneb.astrojumper.screens.PlayScreen;
+import br.uneb.astrojumper.utils.AssetLoader;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.maps.tiled.TiledMap;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class AstroJumper extends Game {
-    public static final int VIRTUAL_WIDTH = 1100;
-    public static final int VIRTUAL_HEIGHT = 680;
-    public static final float PIXELS_PER_METER = 100;
-
     @Override
     public void create() {
-        setScreen(new PlayScreen(this));
+        AssetLoader.load();
+        setScreen(new PlayScreen(this, AssetLoader.get("level1.tmx", TiledMap.class)));
     }
 
     @Override
     public void render() {
         super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        AssetLoader.dispose();
     }
 }
